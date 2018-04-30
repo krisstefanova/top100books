1d46cbf176c98430f8175f86935869c3ec947b2d
Kristina Stefanova <kris.stefanova@gmail.com>
2018-04-30 22:05:59 +1000

    JSONify Goodreads data

--- src/lib/GoodReadsAPI.js
+++ src/lib/GoodReadsAPI.js
@@ -6,18 +6,41 @@ async function getGoodReadsStats(searchBook) {
   const query = queryString.stringify({q:searchBook})
 
   const url = `${process.env.GOODREADS_URL}/search.xml?key=${process.env.GOODREADS_KEY}&q=${query}`
-  const res = await fetch(url);
-  const xml = await res.text();
+  const res = await fetch(url)
+  const book = await xmlToJson(res)
   
+  return presentBook(book)
+}
+
+async function xmlToJson(res) {
+  const xml = await res.text()
   let json
+  
   xml2js(xml, function (err, result) {
     json = result
-  });
-
-  const book = json.GoodreadsResponse.search[0].results[0].work[0]
-  console.log(book);
+  })
   
-  return book
+  return json
+}
+
+function presentBook(goodReadsData) {
+  let bookDetails = {}
+  const bookData = goodReadsData.GoodreadsResponse.search[0].results[0].work[0]
+  
+  bookDetails.bookId = bookData.id[0]._
+  bookDetails.title = bookData.best_book[0].title[0]
+  bookDetails.author = bookData.best_book[0].author[0].name[0]
+  bookDetails.authorId = bookData.best_book[0].author[0].id[0]._
+  bookDetails.rating = bookData.average_rating[0]
+  bookDetails.ratingsCount = bookData.ratings_count[0]._
+  bookDetails.reviewsCount = bookData.text_reviews_count[0]._
+  bookDetails.publicationYear = bookData.original_publication_year[0]._
+  bookDetails.publicationMonth = bookData.original_publication_month[0]._
+  bookDetails.publicationDay = bookData.original_publication_day[0]._
+  bookDetails.image = bookData.best_book[0].image_url[0]
+  bookDetails.smallImage = bookData.best_book[0].small_image_url[0]
+  
+  return bookDetails
 }
 
 module.exports = {
